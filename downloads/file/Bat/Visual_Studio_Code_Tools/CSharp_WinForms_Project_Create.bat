5c9c46e9c2f2e0bcaa561b91600d0ad95e739d47
G4TG5AG <141083593+G4TG5AG@users.noreply.github.com>
2023-09-10 00:07:30 +0900

    Add files via upload

---
+++ downloads/file/Bat/Visual_Studio_Code_Tools/CSharp_WinForms_Project_Create.bat
@@ -0,0 +1,11 @@
+@echo off
+echo;
+echo ===================================
+echo Visual Studio Code Tools
+echo ===================================
+echo Visual Studio Code用のC# WinFormsプロジェクトを作成するツールです。
+set /p ProjectName="プロジェクト名: "
+echo C#プロジェクトを作成しています...
+dotnet new winforms -o "%ProjectName%"
+code %ProjectName%
+echo C#プロジェクトを作成しました。
